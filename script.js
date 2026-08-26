f222f1b86bfa688968e4083968a9c6b7cc0046fb
Ailla Maria Rodrigues Peguim <ailla.peguim@escola.pr.gov.br>
2026-08-25 22:29:08 -0300

    Ok

--- script.js
+++ script.js
@@ -8,18 +8,34 @@ function virarCartao(cartaoElemento) {
 
 // Função para contabilizar os pontos no placar
 function marcarResposta(event, acertou) {
+
   // Impede que o clique no botão desvire o cartão ao mesmo tempo
   event.stopPropagation();
 
+  const cartao = event.target.closest('.cartao');
+
+  // Verifica se este cartão já foi respondido
+  if (cartao.classList.contains('respondido')) {
+    return;
+  }
+
+  // Marca o cartão como respondido
+  cartao.classList.add('respondido');
+
   if (acertou) {
+
     totalAcertos++;
+
     document.getElementById('acertos').textContent = totalAcertos;
+
   } else {
+
     totalErros++;
+
     document.getElementById('erros').textContent = totalErros;
   }
 
   // Desvira o cartão após clicar no botão de resposta
-  const cartao = event.target.closest('.cartao');
   cartao.classList.remove('virado');
+
 }
